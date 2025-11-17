2d18416772910e8408d758c4ee57b2af15b3427e
ThomasChen16 <143221064+ThomasChen16@users.noreply.github.com>
2025-11-17 08:16:13 +0000

    initalized the keywords, operators, and seperators in sets

--- compiler.cs
+++ compiler.cs
@@ -1,1 +1,16 @@
- 
+
+
+// C# Keywords
+HashSet<string> keywords = new HashSet<string> { "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue",
+            "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern",
+            "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface",
+            "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",
+            "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short",
+            "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try",
+            "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while" };
+
+// C# operators
+HashSet<string> operators = new HashSet<string> { "+", "-", "*", "/", "$", "=", "+=", "-=", "*=", "/=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!",
+                                                  "++", "--", "&", "|", "~", "^", "<<", ">>", "??", "?", "is", "as", "typeof" }; 
+// C# seperators
+HashSet<string> seperators = new HashSet<string> { "(", ")", "{", "}", "[", "]", ";", ",", ".", ":", "::", "?", "=>"};
